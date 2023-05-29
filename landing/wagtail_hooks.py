47c0cbe42ecc84566c27c426da0b14baa0f04d9d
gkiserpong <gkiserpongit@gmail.com>
2023-05-29 14:32:34 +0700

    No Menu

--- landing/wagtail_hooks.py
+++ landing/wagtail_hooks.py
@@ -1,5 +1,5 @@
 from wagtail.core import hooks
 
-@hooks.register('construct_main_menu')
-def hide_item(request, menu_items):
+#@hooks.register('construct_main_menu')
+#def hide_item(request, menu_items):
     menu_items[:] = [item for item in menu_items if item.name != 'help']
