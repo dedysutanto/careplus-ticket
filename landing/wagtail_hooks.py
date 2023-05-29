19413f07bb06e5ed599c6598aa8084c2065e9335
gkiserpong <gkiserpongit@gmail.com>
2023-05-29 14:33:16 +0700

    Flow

--- landing/wagtail_hooks.py
+++ landing/wagtail_hooks.py
@@ -2,4 +2,4 @@ from wagtail.core import hooks
 
 #@hooks.register('construct_main_menu')
 #def hide_item(request, menu_items):
-    menu_items[:] = [item for item in menu_items if item.name != 'help']
+#menu_items[:] = [item for item in menu_items if item.name != 'help']
