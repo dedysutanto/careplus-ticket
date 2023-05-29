75476e4269123294d2d55c295f475ec3de3b4af3
gkiserpong <gkiserpongit@gmail.com>
2023-05-29 13:35:41 +0700

    Ticket Counter

--- config/urls.py
+++ config/urls.py
@@ -5,12 +5,13 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
-from landing.views import landing
+from landing.views import landing, counter
 from qrscan.views import qr_scan, pin_entry, scanner
 
 
 urlpatterns = [
     path('', landing, name="landing"),
+    path('counter/', counter, name="counter"),
     path('qr_code/', include('qr_code.urls', namespace="qr_code")),
     path('admin/', admin.site.urls),
     path('login/', include(wagtailadmin_urls)),
--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Verse
 from random import choice
+from tickets.models import Tickets
 
 
 def landing(request):
@@ -9,6 +10,12 @@ def landing(request):
     random_verse = Verse.objects.get(pk=random_pk)
     
     return render(request, 'landing.html', {'verse': random_verse })
+
+
+def counter(request):
+    ticket_count = Tickets.objects.filter(is_used=True).count()
+
+    return render(request, 'counter.html', {'ticket_count': ticket_count})
 
 
     
