487ec5b019063f92ab2968e5432d0a6e3896c6e9
gkiserpong <gkiserpongit@gmail.com>
2023-05-29 13:48:03 +0700

    Check if UUID is valid

--- qrscan/views.py
+++ qrscan/views.py
@@ -11,7 +11,35 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.utils.timezone import now
 from .models import Pin
 from tickets.models import Tickets
+import uuid
 
+
+def is_valid_uuid(uuid_to_test, version=4):
+    """
+    Check if uuid_to_test is a valid UUID.
+    
+     Parameters
+    ----------
+    uuid_to_test : str
+    version : {1, 2, 3, 4}
+    
+     Returns
+    -------
+    `True` if uuid_to_test is a valid UUID, otherwise `False`.
+    
+     Examples
+    --------
+    >>> is_valid_uuid('c9bf9e57-1685-4c89-bafb-ff5af830be8a')
+    True
+    >>> is_valid_uuid('c9bf9e58')
+    False
+    """
+    
+    try:
+        uuid_obj = UUID(uuid_to_test, version=version)
+    except ValueError:
+        return False
+    return str(uuid_obj) == uuid_to_test
 
 # Seat Number
 def get_seat(start, value):
@@ -29,12 +57,15 @@ def qr_scan(request):
         uuid = request.POST.get('id', None)
         force_checkin = request.POST.get('force_checkin', None)
         #print(registrantid)
-        
-        try:
-            ticket = Tickets.objects.get(uuid=uuid)
-            is_found = True
-        except ObjectDoesNotExist:
+
+        if is_valid_uuid(uuid):
             is_found = False
+        
+            try:
+                ticket = Tickets.objects.get(uuid=uuid)
+                is_found = True
+            except ObjectDoesNotExist:
+                is_found = False
         
         if is_found:
             ticket.is_used = True
