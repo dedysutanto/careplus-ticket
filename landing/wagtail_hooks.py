ee39f826c7c63b3e3349dc740de58b3a1717562b
gkiserpong <gkiserpongit@gmail.com>
2023-05-29 14:29:14 +0700

    Kenapa?

--- landing/wagtail_hooks.py
+++ landing/wagtail_hooks.py
@@ -1,5 +1,5 @@
 from wagtail.core import hooks
 
-@hooks.register("construct_settings_menu")
-def hide_user_menu_item(request, menu_items):
-    menu_items[:] = [item for item in menu_items if item.name != "help"]
+@hooks.register('construct_main_menu')
+def hide_explorer_menu_item_from_frank(request, menu_items):
+    menu_items[:] = [item for item in menu_items if item.name != 'help']
