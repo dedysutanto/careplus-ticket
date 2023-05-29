ffb6540d91b716a4b4919aa933cc1afe8d02f590
gkiserpong <gkiserpongit@gmail.com>
2023-05-29 14:38:14 +0700

    Superman

--- landing/wagtail_hooks.py
+++ landing/wagtail_hooks.py
@@ -3,3 +3,5 @@ from wagtail import hooks
 @hooks.register('construct_main_menu')
 def hide_item(request, menu_items):
     menu_items[:] = [item for item in menu_items if item.name != 'help']
+    if not request.user.is_superuser:
+        menu_items[:] = [item for item in menu_items if item.name != 'reports']
