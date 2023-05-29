2e16315dc2653760b64695ab79810629892ec128
gkiserpong <gkiserpongit@gmail.com>
2023-05-29 14:31:55 +0700

    Hide Menu

--- landing/wagtail_hooks.py
+++ landing/wagtail_hooks.py
@@ -1,5 +1,5 @@
 from wagtail.core import hooks
 
 @hooks.register('construct_main_menu')
-def hide_explorer_menu_item_from_frank(request, menu_items):
+def hide_item(request, menu_items):
     menu_items[:] = [item for item in menu_items if item.name != 'help']
