74952a42e3c2a6c51b23a5cadf37eb59846ce823
gkiserpong <gkiserpongit@gmail.com>
2023-05-31 14:44:20 +0700

    Some finetune

--- tickets/wagtail_hooks.py
+++ tickets/wagtail_hooks.py
@@ -12,7 +12,7 @@ class TicketsUsedAdmin(ModelAdmin):
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
     add_to_admin_menu = True  # or False to exclude your model from the menu
-    list_display = ('ticket', 'ticket_number', 'is_used', 'time_used', 'uuid', 'qr_ticket')
+    list_display = ('ticket', 'ticket_number', 'is_used', 'uuid', 'qr_ticket')
     search_fields = ('uuid', 'ticket__name')
 
 
@@ -37,7 +37,7 @@ class TicketsAdmin(ModelAdmin):
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
     add_to_admin_menu = True  # or False to exclude your model from the menu
-    list_display = ('name', 'uuid', 'ticket_class', 'amount', 'created_at')
+    list_display = ('name', 'ticket_class', 'amount', 'created_at')
 #    list_filter = ('uuid',)
     search_fields = ('uuid', 'name')
 
